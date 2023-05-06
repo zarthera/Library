56fc3c15007465c2fe4749511b4eca397d916663
zarthera <ztartera5@gmail.com>
2023-05-05 20:51:31 -0400

    Header and popup window created

---
+++ app.js
@@ -0,0 +1,22 @@
+let addBookBtn = document.querySelector("add-btn")
+let newBookBtn = document.querySelector("new-book-btn")
+
+
+let popupForm = document.querySelector("popup-form")
+let myLibrary = []
+
+function Book(title, author){
+  this.title = form.value.title;
+  this.author = form.value.author;
+  this.pages = form.value.pages;
+  this.read = form.read.checked
+
+}
+
+function addBookToLibrary(book){
+  myLibrary.push(book);
+}
+
+function newBookForm(){
+
+}
