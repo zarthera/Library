2c30d43175058292a383d9c84d91c6c75d5d7603
zarthera <ztartera5@gmail.com>
2023-05-07 22:57:39 -0400

    Added popup form functionality

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-let addBookBtn = document.querySelector("add-btn")
-let newBookBtn = document.querySelector("new-book-btn")
+let addBookBtn = document.querySelector(".add-btn")
+let newBookBtn = document.querySelector(".new-book-btn")
 
 
-let popupForm = document.querySelector("popup-form")
+let popupForm = document.querySelector(".popup-form")
+let closeFormButton = document.querySelector(".close-form-btn")
+
+closeFormButton.addEventListener("click", () => {
+  popupForm.style.transform = "scale(0)"
+  popupForm.style.transition = "ease .4s all"
+})
+
+newBookBtn.addEventListener("click", () =>{
+  popupForm.style.transform = "scale(1)"
+  popupForm.style.scale = "1"
+  popupForm.style.transition = "ease .4s all"
+} )
 let myLibrary = []
 
 function Book(title, author){
